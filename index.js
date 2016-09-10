a5775581b0b7fefd7081f7e2d42c0c94d1ca70d8
EGOIST <0x142857@gmail.com>
2016-09-10 17:11:36 +0800

    fix name in entry

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function (tasks) {
   if (!Array.isArray(tasks)) {
-    return Promise.reject(new TypeError('promise-series only accepts an array of functions'))
+    return Promise.reject(new TypeError('promise.series only accepts an array of functions'))
   }
   return tasks.reduce(function (current, next) {
     return current.then(next)
